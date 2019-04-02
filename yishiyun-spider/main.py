59bcb9ca0b686b76e553161bef15f57bf4752687
chengzhaoyang <526879530@qq.com>
2019-04-02 10:54:49 +0800

    医视云资讯爬虫

---
+++ yishiyun-spider/main.py
@@ -0,0 +1,48 @@
+from dxy_spider import DxySpider
+from ymt_spider import YmtSpider
+from multiprocessing.dummy import Pool as ThreadPool
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S')
+                    #filename='yishiyu_spider.log',
+                    #filemode='a')
+if __name__ == "__main__":
+    dxy_url = [('http://heart.dxy.cn/tag/news', '心血管'), ('http://endo.dxy.cn/tag/news', '内分泌'),
+               ('http://neuro.dxy.cn/tag/news', '神经'), ('http://chest.dxy.cn/tag/news', '呼吸'),
+               ('http://oncol.dxy.cn/tag/news', '肿瘤'), ('http://hematol.dxy.cn/tag/news', '血液'),
+               ('http://gi.dxy.cn/tag/news', '消化'), ('http://infect.dxy.cn/tag/news', '感染'),
+               ('http://orthop.dxy.cn/tag/news', '骨科'), ('http://prs.dxy.cn/tag/news', '整形'),
+               ('http://surg.dxy.cn/tag/news', '普外'), ('http://neurosurg.dxy.cn/tag/news', '神外'),
+               ('http://thorac.dxy.cn/tag/news', '胸外'), ('http://anesth.dxy.cn/tag/news', '麻醉'),
+               ('http://urol.dxy.cn/tag/news', '泌尿'), ('http://ophth.dxy.cn/tag/news', '眼科'),
+               ('http://neph.dxy.cn/tag/news', '肾内'), ('http://rheum.dxy.cn/tag/news', '风湿'),
+               ('http://obgyn.dxy.cn/tag/news', '妇产'), ('http://pediatr.dxy.cn/tag/news', '儿科'),
+               ('http://derm.dxy.cn/tag/news', '皮肤'), ('http://dental.dxy.cn/tag/news', '口腔'),
+               ('http://ccm.dxy.cn/tag/news', '重症'), ('http://rehab.dxy.cn/tag/news', '康复'),
+               ('http://radiol.dxy.cn/tag/news', '影像'), ('http://ultrasound.dxy.cn/tag/news', '超声'),
+               ('http://psych.dxy.cn/tag/news', '精神'), ('http://otol.dxy.cn/tag/news', '耳鼻喉')]
+
+    ymt_url = [('http://news.medlive.cn/heart/info/','心血管内科',1),('http://news.medlive.cn/neuro/info/','神经内科',2),
+               ('http://news.medlive.cn/endocr/info/','内分泌科',5),('http://news.medlive.cn/gi/info/','消化科',3),
+               ('http://news.medlive.cn/liver/info/','肝病科',4),('http://news.medlive.cn/infect/info/','感染科',12),
+               ('http://news.medlive.cn/cancer/info/','肿瘤科',6),('http://news.medlive.cn/hema/info/','血液科',7),
+               ('http://news.medlive.cn/pul/info/','呼吸科',9),('http://news.medlive.cn/neph/info/','肾内科',10),
+               ('http://news.medlive.cn/imm/info/','风湿免疫科',11),('http://news.medlive.cn/psy/info/','精神科',8),
+               ('http://news.medlive.cn/surgery/info/','普通外科',13),('http://news.medlive.cn/ns/info/','神经外科',14),
+               ('http://news.medlive.cn/chest/info/','胸心外科',15),('http://news.medlive.cn/orth/info/','骨科',17),
+               ('http://news.medlive.cn/uro/info/','泌尿外科',16),('http://news.medlive.cn/ps/info/','整形外科',18),
+               ('http://news.medlive.cn/anes/info/','麻醉科',19),('http://news.medlive.cn/obgyn/info/','妇产科',20),
+               ('http://news.medlive.cn/ped/info/','儿科',21),('http://news.medlive.cn/derm/info/','皮肤性病科',25),
+               ('http://news.medlive.cn/oph/info/','眼科',22),('http://news.medlive.cn/ent/info/','耳鼻咽喉科',23),
+               ('http://news.medlive.cn/oral/info/','口腔科',24),('http://news.medlive.cn/em/info/','急诊/重症',26),
+               ('http://news.medlive.cn/xctmr/info/','影像科',27),('http://news.medlive.cn/lab/info/','检验科',28)]
+
+    dxy = DxySpider(first_time=True)
+    ymt = YmtSpider(first_time=True)
+    pool = ThreadPool(8)
+    #pool.map_async(dxy.crawl_page_source, dxy_url)
+    pool.map_async(ymt.crawl_page_source, ymt_url)
+    pool.close()
+    pool.join()
---
+++ yishiyun-spider/ymt_spider.py
@@ -0,0 +1,82 @@
+from meta_spider import Spider
+import requests
+from lxml import etree
+import logging
+import json
+
+class YmtSpider(Spider):
+    """YiMaiTong spider"""
+    def __init__(self, first_time):
+        super(YmtSpider, self).__init__(first_time)
+
+    def crawl_page_source(self, url_dept):
+        url = url_dept[0]
+        dept = url_dept[1]
+        div_type = url_dept[2]
+        # proxies = {
+        #     'http': '127.0.0.1:8080',
+        #     'https': '127.0.0.1:8080'
+        # }
+        if self.first_time:
+            deadLine = self.get_deadline(180)
+        else:
+            deadLine = self.get_deadline(1)
+        createtime = self.get_deadline(0)
+        post_form = {'submit_type': 'ajax','ac': 'research_branch','div_type': 1, 'model_type': 'info','cat_type': 'research','div_type':div_type}
+        post_url = 'http://news.medlive.cn/cms.php'
+        host_url = 'http://news.medlive.cn'
+        headers0 = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"}
+        headers1 = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+                   "Host": "news.medlive.cn",
+                   "Origin": "http://news.medlive.cn",
+                   "Referer": url}
+        for i in range(100):
+            if createtime < deadLine:
+                break
+            try:
+                if i == 0:
+                    response = requests.get(url, headers=headers0)
+                    content = etree.HTML(response.text)
+                    url_list = content.xpath('//*[@id="more"]/div/div[2]/div[1]/a/@href')
+                    createtime_list = content.xpath('//*[@id="more"]/div/div[2]/div[3]/span[1]/span/text()')
+                else:
+                    post_form['page'] = i
+                    response = requests.post(post_url, data=post_form, headers = headers1)
+                    html = json.loads(response.text).get("html")
+                    content = etree.HTML(html)
+                    url_list = content.xpath('/html/body/div/div[2]/div[1]/a/@href')
+                    createtime_list = content.xpath('/html/body/div/div[2]/div[3]/span[1]/span/text()')
+                url_list = [url for url in url_list if 'promotion' not in url]
+                if len(url_list) > 0 and len(url_list) == len(createtime_list):
+                    for i in range(len(url_list)):
+                        createtime = createtime_list[i].strip('\n').strip()
+                        if createtime < deadLine:
+                            break
+                        detail_url = host_url + url_list[i]
+                        detail_response = requests.get(detail_url, headers=headers0)
+                        detail_content = etree.HTML(detail_response.text)
+                        title = detail_content.xpath('//*[@id="content"]/div[1]/div[1]/div/h1/text()')
+                        if len(title) > 0:
+                            title = title[0].strip('\n').strip()
+                            gid = self.parse_title(title)
+                            if self.is_exists(gid):
+                                logging.warning("this article already exists in db!!")
+                                continue
+                            author = '医脉通web'
+                            source = '医脉通web'
+                            content = detail_content.xpath('//*[@id="content"]/div[1]/div[1]/div/div[4]/div[1]')
+                            if len(content) > 0:
+                                content = etree.tostring(content[0], method='html').decode("utf-8")
+                                img_urls = detail_content.xpath('//p/img/@src')
+                                if len(img_urls) > 0:
+                                    content = self.replace_img_url(img_urls, content, source)
+                                data = {'title': title, 'author': author, 'dept': dept, 'source': source,
+                                        'create_time': createtime,
+                                        'gid': gid, 'content': content}
+                                self.save_data2db(data)
+                            else:
+                                logging.warning("the content is empty!!!")
+                        else:
+                            logging.warning("the title is empty!!!")
+            except Exception as e:
+                logging.error("failed to crawl yimaitong:{}".format(e))
