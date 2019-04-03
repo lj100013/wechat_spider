9f277acb75bd97a572588f12d620026115529cd2
chengzhaoyang <526879530@qq.com>
2019-04-03 10:38:36 +0800

    fix

--- yishiyun-spider/main.py
+++ yishiyun-spider/main.py
@@ -7,7 +7,7 @@ import sys
 import argparse
 logging.basicConfig(level=logging.WARNING,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S'
+                    datefmt='%a, %d %b %Y %H:%M:%S',
                     filename='yishiyu_spider.log',
                     filemode='a')
 if __name__ == "__main__":
