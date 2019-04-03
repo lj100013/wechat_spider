16beaa457acd18644bff6fe8474d8dfe594bcd08
lin <linz@dachentech.com.cn>
2019-04-03 17:06:31 +0800

    sogou 升级反爬处理

--- weixin/setting.py
+++ weixin/setting.py
@@ -1,32 +1,86 @@
+urls = ["每日医学资讯", "医师报", "生物谷", "看医界",
+            "丁香园", "好医生", "儿科时间", "医学界消化肝病频道", "小大夫漫画", "消化界", "MedSci梅斯",
+            "华医网", "IBD学术情报官", "爱肝联盟", "临床肝胆病杂志", "消化时间", "医脉通消化科",
+            "孙锋医生", "基层医师公社", "医学界", "医学界心血管频道", "心血管时间","心在线", "中国循环杂志", "医脉通心内频道",
+            "哈特瑞姆心脏之声","医学之声", "医学界急诊与重症频道", "医学界神经病学频道",
+            "神经时间","神经病学俱乐部", "医脉通神经科", "国际眼科时讯", "医信眼科", "视远惟明▪惟视眼科", "泌尿科那点事儿",
+            "医学界外科频道", "中国实用外科杂志", "医学界儿科频道", "国际儿科学杂志", "医学界精神病学频道", "儿科学大查房",
+            "人卫儿科", "中国儿科前沿论坛", "皮肤时间", "实用皮肤病学杂志", "医脉通肾内频道", "透析圈", "感染时间", "SIFIC感染官微", "医脉通抗感染",
+            "医学界感染频道", "中华医学网","海上柳叶刀", "医学内刊", "内分泌时间", "神经科技", "医学界风湿免疫频道", "医学界临床药学频道", "老虎讲骨",
+            "放疗时空", "医学界检验频道", "医学界影像诊断与介入频道", "放射沙龙", "肿瘤时间", "医学界肿瘤频道", "感染科空间", "SIFIC感染科普笔记",
+            "下夜班", "医闻速递", "三甲传真", "创新医学网", "国际肝胆胰疾病杂志", "胃肠肿瘤外科", "中华儿科杂志", "儿科空间", "中国实用儿科杂志",
+            "皮肤科钟华", "CSDCMA皮科时讯论坛", "医生汇心血管论坛", "急诊医学资讯", "中国急救医学杂志", "急诊时间", "中国小儿急救医学",
+            "精神时间", "精神康复", "医脉通精神科", "大话精神", "神经科的那些事", "神经医学社区", "神经脊柱时讯", "医学界呼吸频道", "中国眼科医生",
+            "SIFIC感染循证资讯", "中华消化外科杂志", "朝阳心脏超声", "危重症文献学习", "中华重症医学电子杂志", "神经现实", "神经介入资讯",
+            "医药魔方", "赛博蓝", "中洪博元医学实验帮", "医咖会", "生物学霸"
+            ]
 
-MYSQL_HOST = '192.168.3.154'
-MYSQL_USER = 'root'
-LOG_LEVEL = 'WARNING'
+name_keyword = {
+    "好医生": "QK", "丁香园": "QK", "看医界": "INFO", "医学界消化肝病频道": "NKAC", "小大夫漫画": "QK", "消化界": "NKAC", "MedSci梅斯": "QK",
+    "华医网": "QK", "IBD学术情报官": "NKAC", "爱肝联盟": "CRAC", "临床肝胆病杂志": "WKAB", "消化时间": "NKAC", "医脉通消化科": "NKAC",
+    "孙锋医生": "WKAC", "基层医师公社": "QK", "医学界": "QK", "医学界心血管频道": "NKAA", "心血管时间": "NKAA", "心在线": "NKAA",
+    "中国循环杂志": "NKAA", "医脉通心内频道": "NKAA", "哈特瑞姆心脏之声": "NKAA", "医学之声": "QK", "医学界急诊与重症频道": "JZ",
+    "医路向前巍子": "JZ", "医学界神经病学频道": "NKAB", "神经时间": "NKAB", "神经病学俱乐部": "NKAB", "医脉通神经科": "NKAB", "国际眼科时讯": "YK",
+    "医信眼科": "YK", "眼视光观察": "YK", "视远惟明▪惟视眼科": "YK", "医脉通泌尿外科": "WKAA", "泌尿科那点事儿": "WKAA", "泌尿外科郭医生": "WKAA",
+    "医学界外科频道": "WKAG", "中国实用外科杂志": "WKAC", "儿科时间": "EK", "医学界儿科频道": "EK", "国际儿科学杂志": "EK", "医学界精神病学频道": "JS",
+    "儿科学大查房": "EKAB", "人卫儿科": "EKAB", "中国儿科前沿论坛": "EKAB", "皮肤时间": "PFAB", "实用皮肤病学杂志": "PFAB",
+    "肝胆外科": "WKAB", "肾内时间": "NKAL", "医脉通肾内频道": "NKAL", "透析圈": "NKAL", "感染时间": "NKAJ", "SIFIC感染官微": "NKAJ",
+    "医脉通抗感染": "NKAJ",
+    "医学界感染频道": "NKAJ", "中华医学网": "NKAM", "海上柳叶刀": "NKAM", "医学内刊": "NKAM", "每日医学资讯": "INFO", "医师报": "INFO",
+    "医脉通": "INFO", "健康界": "INFO", "生物谷": "INFO", "看医界": "INFO", "内分泌时间": "NKAD", "神经科技": "WKAH", "医学界风湿免疫频道": "NKAE",
+    "医学界临床药学频道": "QK", "老虎讲骨": "WKAR", "放疗时空":"JY", "医学界检验频道":"JY", "医学界影像诊断与介入频道":"JY", "放射沙龙":"JY", "健康点healthpoint":"WKAT",
+    "肿瘤时间":"WKAT", "医学界肿瘤频道":"WKAT", "感染科空间":"NKAJ", "SIFIC感染科普笔记":"NKAJ", "下夜班":"QK", "医闻速递":"INFO", "三甲传真":"INFO", "创新医学网":"QK",
+    "爱肝一生微课堂": "CRAC", "国际肝胆胰疾病杂志": "CRAC", "胃肠肿瘤外科": "WKAC", "儿科助手":"EK", "中华儿科杂志":"EKAB", "儿科空间":"EK", "中国实用儿科杂志":"EK",
+    "皮肤科钟华":"PFAB", "CSDCMA皮科时讯论坛":"PFAB", "医生汇心血管论坛": "NKAA", "医脉通急诊重症科": "JZ", "急诊医学资讯": "JZ", "中国急救医学杂志": "JZ",
+    "急诊时间": "JZ", "中国小儿急救医学": "JZ", "重症医学": "JZ", "精神时间":"JS", "精神康复":"JS", "医脉通精神科":"JS", "大话精神":"JS", "神经科的那些事":"NKAB",
+    "神经医学社区":"NKAB", "神经脊柱时讯": "NKAB", "医学界呼吸频道": "NKAF", "中国眼科医生": "YK", "SIFIC感染循证资讯": "NKAJ", "中华消化外科杂志": "NKAC",
+    "朝阳心脏超声": "NKAA", "危重症文献学习": "ZZ", "中华重症医学电子杂志": "ZZ", "神经现实": "SJ", "神经介入资讯": "SJ",
+    "医药魔方": "INFO", "赛博蓝": "INFO", "中洪博元医学实验帮": "INFO", "医咖会": "INFO", "生物学霸": "INFO"
+}
 
-# 你自己数据库的密码
-MYSQL_PASSWORD = 'Dachen@222'
-MYSQL_PORT = 3306
+base_url = "https://weixin.sogou.com/weixin?type=1&s_from=input&query="
+sogou_header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+    "Host": "weixin.sogou.com"
+}
 
-# 你自己数据库的名称
-MYSQL_DB = 'wordpress'
-CHARSET = 'utf8'
+weixin_header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+    "Host": "mp.weixin.qq.com"
+}
 
-# 数据库连接信息
-# connections = {
-#     True: {  # debug
-#         'host': '192.168.3.121',
-#         'user': 'root',
-#         'passwd': 'dachen@123',
-#         'db': 'wordpress',
-#         'charset': 'utf8'
-#     },
-#     False: {  # online
-#         'host': '192.168.3.154',
-#         'user': 'root',
-#         'passwd': 'Dachen@222',
-#         'db': 'wordpress',
-#         'charset': 'utf8'
-#     }
-# }
+main_headers = {
+            "Host": "weixin.sogou.com",
+            "Connection": "Keep-Alive",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+            "Referer": "https://weixin.sogou.com/weixin?type=1&query=%E6%AF%8F%E6%97%A5%E5%8C%BB%E5%AD%A6%E8%B5%84%E8%AE%AF&ie=utf8&s_from=input&_sug_=n&_sug_type_=1&w=01015002&oq=&ri=99&sourceid=sugg&sut=0&sst0=1554099429071&lkt=0%2C0%2C0&p=40040108",
+            "Cookie": "SUID=A7B00FB72F20910A000000005ABC4EC2; SUV=1522290377774645; SMYUV=1522290377776035; CXID=2D6311EC7124CB182D9C4AC5D01C58C3; wuid=AAEHfWwQIQAAAAqGCmK1ZwkAGwY=; ssuid=2994604160; pex=C864C03270DED3DD8A06887A372DA219231FFAC25A9D64AE09E82AED12E416AC; ABTEST=0|1554087014|v1; IPLOC=CN4403; JSESSIONID=aaarADVkUHXuy5wDUBjNw; PHPSESSID=vhfc0ks1keok0d7sn8fgp3p9e0; weixinIndexVisited=1; SNUID=1126C3483035B55AA14BEF3A30D65EF6; sct=541",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
+        }
 
-debug = False
+phantomJS_path = '/data/program/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
+#phantomJS_path = 'E:\\python\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe'
+zhi_ma_apiUrl = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&pack=34734&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions="
+blank_jpeg_img = "NzKkzoeG5s2FcdXyTKYAErrs5QNBXGVS75aQnxYX1RmPQTRwN3CsZw5Dfjb3oiaYLjgNaWrFck8rJJRoqHxuItA.jpeg"
+driver_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36"
+
+# MYSQL_HOST = '192.168.3.154'
+# MYSQL_USER = 'root'
+# LOG_LEVEL = 'WARNING'
+# MYSQL_PASSWORD = 'Dachen@222'
+# MYSQL_PORT = 3306
+# MYSQL_DB = 'wordpress'
+# CHARSET = 'utf8'
+
+#debug
+#img_base_url = 'http://bigdata.dev.file.dachentech.com.cn/'
+#online
+#img_base_url = 'http://content.file.mediportal.com.cn/'
+#debug
+#qiniu_service_url = 'http://192.168.2.108:8076/qiniu/upload/bigdata'
+#online
+#qiniu_service_url = 'http://xg.mediportal.com.cn/docapi/qiniu/upload/content?access_token=36e93fe6391d4e638635df2c286e73da'
+#debug = True
--- weixin/spider_wechart.py
+++ weixin/spider_wechart.py
@@ -1,64 +1,60 @@
 # -*- coding:utf-8 -*-
 from db import *
+from setting import *
 from utils import *
 import time
 from multiprocessing.dummy import Pool as ThreadPool
 import os
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
+import math
+import random
+import configparser
+conf = configparser.ConfigParser()
+#conf.read(r"E:\job_script\utils\config.ini")
+conf.read("/data/job_pro/utils/config.ini")
+img_base_url=conf.get('weixin', 'img_base_url')
 import urllib3
 urllib3.disable_warnings()
-
-base_url = "https://weixin.sogou.com/weixin?type=1&s_from=input&query="
-headers0 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-    "Host": "weixin.sogou.com"
-}
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-    "Host": "mp.weixin.qq.com"
-}
-
-name_keyword = {
-    "好医生": "QK", "丁香园": "QK", "看医界": "INFO", "医学界消化肝病频道": "NKAC", "小大夫漫画": "QK", "消化界": "NKAC", "MedSci梅斯": "QK",
-    "华医网": "QK", "IBD学术情报官": "NKAC", "爱肝联盟": "CRAC", "临床肝胆病杂志": "WKAB", "消化时间": "NKAC", "医脉通消化科": "NKAC",
-    "孙锋医生": "WKAC", "基层医师公社": "QK", "医学界": "QK", "医学界心血管频道": "NKAA", "心血管时间": "NKAA", "心在线": "NKAA",
-    "中国循环杂志": "NKAA", "医脉通心内频道": "NKAA", "哈特瑞姆心脏之声": "NKAA", "医学之声": "QK", "医学界急诊与重症频道": "JZ",
-    "医路向前巍子": "JZ", "医学界神经病学频道": "NKAB", "神经时间": "NKAB", "神经病学俱乐部": "NKAB", "医脉通神经科": "NKAB", "国际眼科时讯": "YK",
-    "医信眼科": "YK", "眼视光观察": "YK", "视远惟明▪惟视眼科": "YK", "医脉通泌尿外科": "WKAA", "泌尿科那点事儿": "WKAA", "泌尿外科郭医生": "WKAA",
-    "医学界外科频道": "WKAG", "中国实用外科杂志": "WKAC", "儿科时间": "EK", "医学界儿科频道": "EK", "国际儿科学杂志": "EK", "医学界精神病学频道": "JS",
-    "儿科学大查房": "EKAB", "人卫儿科": "EKAB", "中国儿科前沿论坛": "EKAB", "皮肤时间": "PFAB", "实用皮肤病学杂志": "PFAB",
-    "肝胆外科": "WKAB", "肾内时间": "NKAL", "医脉通肾内频道": "NKAL", "透析圈": "NKAL", "感染时间": "NKAJ", "SIFIC感染官微": "NKAJ",
-    "医脉通抗感染": "NKAJ",
-    "医学界感染频道": "NKAJ", "中华医学网": "NKAM", "海上柳叶刀": "NKAM", "医学内刊": "NKAM", "每日医学资讯": "INFO", "医师报": "INFO",
-    "医脉通": "INFO", "健康界": "INFO", "生物谷": "INFO", "看医界": "INFO", "内分泌时间": "NKAD", "神经科技": "WKAH", "医学界风湿免疫频道": "NKAE",
-    "医学界临床药学频道": "QK", "老虎讲骨": "WKAR", "放疗时空":"JY", "医学界检验频道":"JY", "医学界影像诊断与介入频道":"JY", "放射沙龙":"JY", "健康点healthpoint":"WKAT",
-    "肿瘤时间":"WKAT", "医学界肿瘤频道":"WKAT", "感染科空间":"NKAJ", "SIFIC感染科普笔记":"NKAJ", "下夜班":"QK", "医闻速递":"INFO", "三甲传真":"INFO", "创新医学网":"QK",
-    "爱肝一生微课堂": "CRAC", "国际肝胆胰疾病杂志": "CRAC", "胃肠肿瘤外科": "WKAC", "儿科助手":"EK", "中华儿科杂志":"EKAB", "儿科空间":"EK", "中国实用儿科杂志":"EK",
-    "皮肤科钟华":"PFAB", "CSDCMA皮科时讯论坛":"PFAB", "医生汇心血管论坛": "NKAA", "医脉通急诊重症科": "JZ", "急诊医学资讯": "JZ", "中国急救医学杂志": "JZ",
-    "急诊时间": "JZ", "中国小儿急救医学": "JZ", "重症医学": "JZ", "精神时间":"JS", "精神康复":"JS", "医脉通精神科":"JS", "大话精神":"JS", "神经科的那些事":"NKAB",
-    "神经医学社区":"NKAB", "神经脊柱时讯": "NKAB", "医学界呼吸频道": "NKAF", "中国眼科医生": "YK", "SIFIC感染循证资讯": "NKAJ", "中华消化外科杂志": "NKAC",
-    "朝阳心脏超声": "NKAA", "危重症文献学习": "ZZ", "中华重症医学电子杂志": "ZZ", "神经现实": "SJ", "神经介入资讯": "SJ",
-    "医药魔方": "INFO", "赛博蓝": "INFO", "中洪博元医学实验帮": "INFO", "医咖会": "INFO", "生物学霸": "INFO"
-}
 
 
 ip_count = 0
 article_count = 0
 
-
+# 获取IP的API接口
 def get_ip():
-    # 获取IP的API接口
     time.sleep(2)
-    apiUrl = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&pack=34734&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions="
     # 获取IP时间间隔，建议为5秒
-    res = requests.get(apiUrl).text
+    res = requests.get(zhi_ma_apiUrl).text
     global ip_count
     ip_count = ip_count + 1
     print('代理ip数: ' + str(ip_count))
     return res
 
+def format_url(url, text, proxies):
+    def _parse_url(url, pads):
+        b = math.floor(random.random() * 100) + 1
+        a = url.find("url=")
+        c = url.find("&k=")
+        if a != -1 and c == -1:
+            a = url[a + sum([int(i) for i in pads]) + b]
+        return '{}&k={}&h={}'.format(url, b, a)
+
+    if url.startswith('/link?url='):
+        url = 'https://weixin.sogou.com{}'.format(url)
+        patt = r'href\.substr\(a\+(\d+)\+parseInt\("(\d+)"\)\+b,1\)'
+        pads = re.findall(patt, text)
+        url = _parse_url(url, pads[0] if pads else [])
+        response = requests.get(url, headers=main_headers, verify=False, proxies=proxies)
+        content = response.text
+        pattern = r"'([\s\S]*?)'"
+        id_node = re.findall(pattern, content)
+        uri = ''.join(id_node)
+        url = uri.replace('@', '')
+    else:
+        print("## get mail url wrong #")
+
+    return url
 
 def spider_wechat(name):
     mail_url = base_url + name
@@ -70,11 +66,14 @@ def spider_wechat(name):
     port = ips.split(":")[1]
     null_url = True
     wechart_url = ''
+    text = ''
+    wechat_ip_count = 0
 
     while null_url:
         try:
-            response = requests.get(mail_url, headers=headers0, proxies=proxies)
-            content = etree.HTML(response.text)
+            response = requests.get(mail_url, headers=sogou_header, proxies=proxies)
+            text = response.text
+            content = etree.HTML(text)
             urls = content.xpath('//*[@id="sogou_vr_11002301_box_0"]/div/div[2]/p[1]/a/@href')
             if len(urls) > 0:
                 wechart_url = urls[0]
@@ -82,6 +81,7 @@ def spider_wechat(name):
             else:
                 new_ips = get_ip().strip()
                 print("重新获取代理ip：{}".format(new_ips))
+                wechat_ip_count += 1
                 proxies = {"https": "https://" + new_ips}
                 online_ips = new_ips.split(":")
                 if len(online_ips) > 1:
@@ -91,16 +91,20 @@ def spider_wechat(name):
             print(e)
             new_ips = get_ip().strip()
             print("重新获取代理ip：{}".format(new_ips))
+            wechat_ip_count += 1
             proxies = {"https": "https://" + new_ips}
             online_ips = new_ips.split(":")
             if len(online_ips) > 1:
                 host = online_ips[0]
                 port = online_ips[1]
 
-    phantomJSdriver = '/data/program/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
+        if wechat_ip_count > 15:
+            return 1
+
+    phantomJSdriver = phantomJS_path
     os.environ["webdriver.phantomjs.driver"] = phantomJSdriver
     dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36")
+    dcap["phantomjs.page.settings.userAgent"] = (driver_user_agent)
     no_article = True
     wechart_urls = []
     wechat_ip_count = 0
@@ -112,8 +116,9 @@ def spider_wechat(name):
         driver.set_script_timeout(20)
 
         try:
-            driver.get(wechart_url)
-            time.sleep(5)
+            a_url = format_url(wechart_url, text, proxies)
+            driver.get(a_url)
+            time.sleep(2)
             content = driver.page_source
             page = etree.HTML(content)
             n_urls = page.xpath('//h4[@class="weui_media_title"]/@hrefs')
@@ -151,7 +156,7 @@ def spider_wechat(name):
             driver.quit()
 
         if wechat_ip_count > 12:
-            break
+            return 1
 
     for line in wechart_urls:
         detail_url = line.strip()
@@ -159,7 +164,7 @@ def spider_wechat(name):
             continue
 
         try:
-            result = requests.get(url=detail_url, headers=headers, proxies=proxies)
+            result = requests.get(url=detail_url, headers=weixin_header, proxies=proxies)
             result.encoding = 'utf-8'
             url_content = result.text
         except Exception as e:
@@ -213,10 +218,7 @@ def spider_wechat(name):
             elif '=gif' in link:
                 img_format = '.gif'
 
-            if setting.debug:
-                first_string = 'src="http://bigdata.dev.file.dachentech.com.cn/' + fname + img_format
-            else:
-                first_string = 'src="http://content.file.mediportal.com.cn/' + fname + img_format
+            first_string = 'src="' + img_base_url + fname + img_format
             origian_first = 'data-src="' + link
             content = content.replace(origian_first, first_string)
 
@@ -237,11 +239,8 @@ def spider_wechat(name):
 
         blank_urls = re.findall(blank_pattern, content)
         for blank_img in blank_urls:
-            file_name = "NzKkzoeG5s2FcdXyTKYAErrs5QNBXGVS75aQnxYX1RmPQTRwN3CsZw5Dfjb3oiaYLjgNaWrFck8rJJRoqHxuItA.jpeg"
-            if setting.debug:
-                first_string = 'http://bigdata.dev.file.dachentech.com.cn/' + file_name
-            else:
-                first_string = 'http://content.file.mediportal.com.cn/' + file_name
+            file_name = blank_jpeg_img
+            first_string = img_base_url + file_name
             blank_img = blank_img.replace('(', '')
             blank_img = blank_img.replace(')', '')
             content = content.replace(blank_img, first_string)
@@ -284,23 +283,6 @@ def spider_wechat(name):
 
 
 if __name__ == '__main__':
-    urls = ["每日医学资讯", "医师报", "医脉通", "健康界", "生物谷", "看医界",
-            "丁香园", "好医生", "儿科时间", "医学界消化肝病频道", "小大夫漫画", "消化界", "MedSci梅斯",
-            "华医网", "IBD学术情报官", "爱肝联盟", "临床肝胆病杂志", "消化时间", "医脉通消化科",
-            "孙锋医生", "基层医师公社", "医学界", "医学界心血管频道", "心血管时间","心在线", "中国循环杂志", "医脉通心内频道",
-            "哈特瑞姆心脏之声","医学之声", "医学界急诊与重症频道", "医学界神经病学频道",
-            "神经时间","神经病学俱乐部", "医脉通神经科", "国际眼科时讯", "医信眼科", "视远惟明▪惟视眼科", "医脉通泌尿外科","泌尿科那点事儿",
-            "医学界外科频道", "中国实用外科杂志", "医学界儿科频道", "国际儿科学杂志", "医学界精神病学频道", "儿科学大查房",
-            "人卫儿科", "中国儿科前沿论坛", "皮肤时间", "实用皮肤病学杂志", "肾内时间", "医脉通肾内频道", "透析圈", "感染时间", "SIFIC感染官微", "医脉通抗感染",
-            "医学界感染频道", "中华医学网","海上柳叶刀", "医学内刊", "内分泌时间", "神经科技", "医学界风湿免疫频道", "医学界临床药学频道", "老虎讲骨", 
-            "放疗时空", "医学界检验频道", "医学界影像诊断与介入频道", "放射沙龙", "肿瘤时间", "医学界肿瘤频道", "感染科空间", "SIFIC感染科普笔记",
-            "下夜班", "医闻速递", "三甲传真", "创新医学网", "爱肝一生微课堂", "国际肝胆胰疾病杂志", "胃肠肿瘤外科", "中华儿科杂志", "儿科空间", "中国实用儿科杂志",
-            "皮肤科钟华", "CSDCMA皮科时讯论坛", "医生汇心血管论坛", "医脉通急诊重症科", "急诊医学资讯", "中国急救医学杂志", "急诊时间", "中国小儿急救医学",
-            "精神时间", "精神康复", "医脉通精神科", "大话精神", "神经科的那些事", "神经医学社区", "神经脊柱时讯", "医学界呼吸频道", "中国眼科医生",
-            "SIFIC感染循证资讯", "中华消化外科杂志", "朝阳心脏超声", "危重症文献学习", "中华重症医学电子杂志", "神经现实", "神经介入资讯",
-            "医药魔方", "赛博蓝", "中洪博元医学实验帮", "医咖会", "生物学霸"
-            ]
-
     pool = ThreadPool(4)
     pool.map(spider_wechat, urls)
     pool.close()
