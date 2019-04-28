efbd7301e86e461a9d6a60cc59e22f21b5a0d521
chengzhaoyang <526879530@qq.com>
2019-04-28 16:25:13 +0800

    医脉通增加代理ip

--- yishiyun-spider/main.py
+++ yishiyun-spider/main.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     first_time = args.first_time
     dxy = DxySpider(first_time=first_time)
     ymt = YmtSpider(first_time=first_time)
-    pool = ThreadPool(8)
+    pool = ThreadPool(4)
     pool.map_async(dxy.crawl_page_source, dxy_url)
     pool.map_async(ymt.crawl_page_source, ymt_url)
     pool.close()
---
+++ yishiyun-spider/utils.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import time
+import logging
+
+def get_ip(retrytimes):
+    """
+    get proxy ip
+    :return: host:port
+    """
+    zhi_ma_apiUrl = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&pack=34734&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions="
+    try:
+        if retrytimes > 0:
+            res = requests.get(zhi_ma_apiUrl).text
+            json.loads(res)
+            time.sleep(3)
+            return get_ip(retrytimes - 1)
+        else:
+            return ""
+    except:
+        logging.warning("获取代理ip成功{}".format(res))
+        return res.strip('\r\n')
