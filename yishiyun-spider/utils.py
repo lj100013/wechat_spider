42a821c291ed02b1214551e54fff6e01b7a9ecaf
chengzhaoyang <526879530@qq.com>
2019-04-28 18:16:21 +0800

    fix

--- yishiyun-spider/utils.py
+++ yishiyun-spider/utils.py
@@ -9,6 +9,7 @@ def get_ip(retrytimes):
     :return: host:port
     """
     zhi_ma_apiUrl = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&pack=34734&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions="
+    res = ''
     try:
         if retrytimes > 0:
             res = requests.get(zhi_ma_apiUrl).text
