add943ca5ba316925b3d90ecee3e94a7147d301e
luolianyun <luoly@dachentech.com.cn>
2019-05-14 11:00:37 +0800

    【mongo2hive改造】run.sh修改，添加传参

--- mongo2hive/run.sh
+++ mongo2hive/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if [[ $1 == '' ]];then
+if [[ $2 == '' ]];then
    preday=`date -d "yesterday" +%Y-%m-%d`
    pre2day=`date -d "2 days ago" +%Y-%m-%d`
 else
-   preday=$1
-   pre2day=$2
+   preday=$2
+   pre2day=$3
 fi
 
-bash /data/job_pro/utils/job_flag.sh mongo2hive /data/job_pro/dataX/mongo2hive $preday $pre2day
+jobname=$1
+
+bash /data/job_pro/utils/job_flag.sh $jobname /data/job_pro/dataX/mongo2hive $preday $pre2day
