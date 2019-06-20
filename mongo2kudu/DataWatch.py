51cc5a9d398519a672258d807895d5e52498ebf0
chenlg <chenlg@dachentech.com.cn>
2019-06-20 19:51:44 +0800

    fix

--- mongo2kudu/DataWatch.py
+++ mongo2kudu/DataWatch.py
@@ -15,93 +15,84 @@ CONF_PORT = conf.get('conf_mysql', 'port')
 CONF_UN = conf.get('conf_mysql', 'username')
 CONF_PW = conf.get('conf_mysql', 'password')
 CONF_DB = conf.get('conf_mysql', 'database')
-database = sys.argv[1].split('.')[0]
-table = sys.argv[1].split('.')[1]
 
 # 导入mysql配置表
 config_con = pymysql.connect(host=CONF_HOST, port=int(CONF_PORT), user=CONF_UN, password=CONF_PW, db=CONF_DB)
 config_cur = config_con.cursor()
-try:
-    sql = "select * from test.KuduETL_config where kudu_db='%s' and kudu_collection='%s';" % (database,table)
-    config_cur.execute(sql)
-    results = config_cur.fetchone()
-    # print(results)
-    source_type = results[0]
-    source_host = results[1]
-    source_port = results[2]
-    source_username = results[3]
-    source_password = results[4]
-    source_db = results[5]
-    source_collection = results[6]
-    kudu_host = results[7]
-    kudu_port = results[8]
-    kudu_db = results[9]
-    kudu_collection = results[10]
-except:
-   print ("Error: unable to fetch data")
-finally:
-    config_con.close()
 
-# 统计来源数据
-if source_type == 'mongo':
-    mongo_con = pymongo.MongoClient(host=source_host, port=int(source_port), username=source_username, password=source_password)
-    mongo_db = mongo_con.get_database(source_db)
-    mongo_collection = mongo_db.get_collection(source_collection)
-    try:
+sql = "select * from KuduETL_Config"
+config_cur.execute(sql)
+results = config_cur.fetchall()
+d = ""
+for row in results:
+    source_type = row[0]
+    source_host = row[1]
+    source_port = row[2]
+    source_username = row[3]
+    source_password = row[4]
+    source_db = row[5]
+    source_collection = row[6]
+    kudu_host = row[7]
+    kudu_port = row[8]
+    kudu_db = row[9]
+    kudu_collection = row[10]
+
+    # 统计来源数据
+    if source_type == 'mongo':
+        mongo_con = pymongo.MongoClient(host=source_host, port=int(source_port), username=source_username,
+                                        password=source_password)
+        mongo_db = mongo_con.get_database(source_db)
+        mongo_collection = mongo_db.get_collection(source_collection)
         source_count = mongo_collection.find().count()
-    except:
-        print("Error: unable to fetch data")
-    finally:
         mongo_con.close()
-elif source_type == 'mysql':
-    mysql_con = pymysql.connect(host=source_host, port=int(source_port), user=source_username, password=source_password,db=source_db)
-    mysql_cur = mysql_con.cursor()
-    try:
+    elif source_type == 'mysql':
+        mysql_con = pymysql.connect(host=source_host, port=int(source_port), user=source_username,
+                                    password=source_password, db=source_db)
+        mysql_cur = mysql_con.cursor()
         sql = "select count(*) from %s" % (source_collection)
         mysql_cur.execute(sql)
         source_count = mysql_cur.fetchone()[0]
-    except:
-        print("Error: unable to fetch data")
-    finally:
         mysql_con.close()
 
-# 统计kudu数据
-kudu_con = connect(host=kudu_host, port=int(kudu_port),user='hive',password='hive')
-kudu_cur = kudu_con.cursor()
-sql = 'select count(*) from %s.%s;' % (kudu_db,kudu_collection)
-kudu_cur.execute(sql)
-kudu_count = kudu_cur.fetchall()[0][0]
+    # 统计kudu数据
+    kudu_con = connect(host=kudu_host, port=int(kudu_port), user='hive', password='hive')
+    kudu_cur = kudu_con.cursor()
+    sql = 'select count(*) from %s.%s;' % (kudu_db, kudu_collection)
+    kudu_cur.execute(sql)
+    kudu_count = kudu_cur.fetchall()[0][0]
 
-date =  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-difference = abs(source_count-kudu_count)
+    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    difference = abs(source_count - kudu_count)
 
-# 如果不一致，发送邮件
-if difference != 0:
+    # 如果不一致,增加一行
+    d = d + '<tr>' \
+        '<td><center>%s</center></td>' \
+        '<td><center>%s</center></td>' \
+        '<td><center>%s.%s</center></td>' \
+        '<td><center>%s</center></td>' \
+        '<td><center>%s.%s</center></td>' \
+        '<td><center>%s</center></td>' \
+        '<td><font color="#FF0000"><center>%s</center><font></td>' \
+        '</tr>' % (date,source_type.upper(),source_db,source_collection,source_count,kudu_db,kudu_collection,kudu_count,difference)
+
+# 如果表不为空,发送邮件
+if d != "":
     mailserver = "smtp.163.com"
     username_send = 'kuduconfig@163.com'
     password = 'a123456'
     username_recv = ['kuduconfig@163.com', 'user2@dachentech.com.cn']
-    content=  '<table color="CCCC33" width="800" border="1" cellspacing="0" cellpadding="5" text-align="center">' \
-                        '<tr>' \
-                                '<td style="background:#000; color:#FFF"><center><b>统计时间</b></center></td>' \
-                                '<td style="background:#000; color:#FFF"><center><b>源表类型</b></center></td>' \
-                                '<td style="background:#000; color:#FFF"><center><b>源表</b></center></td>' \
-                                '<td style="background:#000; color:#FFF"><center><b>源表总数</b></center></td>' \
-                                '<td style="background:#000; color:#FFF"><center><b>目标表</b></center></td>' \
-                                '<td style="background:#000; color:#FFF"><center><b>目标表总数</b></center></td>' \
-                                '<td style="background:#000; color:#FFF"><center><b>差值</b></center></td>' \
-                        '</tr>' \
-                        '<tr>' \
-                                '<td><center>%s</center></td>' \
-                                '<td><center>%s</center></td>' \
-                                '<td><center>%s.%s</center></td>' \
-                                '<td><center>%s</center></td>' \
-                                '<td><center>%s.%s</center></td>' \
-                                '<td><center>%s</center></td>' \
-                                '<td><font color="#FF0000"><center>%s</center><font></td>' \
-              '</tr>' \
-              '</table>' % (date,source_type.upper(),source_db,source_collection,source_count,kudu_db,kudu_collection,kudu_count,difference)
-    mail = MIMEText(content,"html","utf-8")
+    content = '<table width="800" border="1" cellspacing="0" cellpadding="5" text-align="center">' \
+              '<tr>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>统计时间</b></center></td>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>源表类型</b></center></td>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>源表</b></center></td>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>源表总数</b></center></td>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>目标表</b></center></td>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>目标表总数</b></center></td>' \
+              '<td style="background:#BEBEBE; color:#000"><center><b>差值</b></center></td>' \
+              '</tr>' + d + \
+              '</table>'
+    mail = MIMEText(content, "html", "utf-8")
     mail['Subject'] = '错误：数据不一致'
     mail['From'] = username_send
     mail['To'] = ','.join(username_recv)
@@ -115,3 +106,8 @@ if difference != 0:
         print("发送失败")
     finally:
         smtp.quit()
+# print(results)
+config_con.close()
+
+
+
