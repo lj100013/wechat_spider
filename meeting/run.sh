374786eaa25e2d2a55136f00c19d6964d4c59b85
luolianyun <luoly@dachentech.com.cn>
2019-07-15 15:52:26 +0800

    【meeting】视频会议相关脚本（9）

--- meeting/run.sh
+++ meeting/run.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 if [ $1 == '' ];then
-   python /data/job_pro/dataX/meeting/kafka2kudu_meeting.py
+   python /data/job_pro/dataX/meeting/kafka2kafka_meeting.py
 else
    echo '=========================='
    echo $1
    echo $2
    echo '=========================='
-   python /data/job_pro/dataX/meeting/kafka2kudu_meeting.py $1 $2
+   python /data/job_pro/dataX/meeting/kafka2kafka_meeting.py $1 $2
 fi
