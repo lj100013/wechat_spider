2e35f687c2ccce8fa29ed69e3ee3212c09fb0428
luolianyun <luoly@dachentech.com.cn>
2019-07-17 14:01:53 +0800

    【meeting】视频会议相关脚本（10）

--- meeting/run_retryurl.sh
+++ meeting/run_retryurl.sh
@@ -8,7 +8,7 @@ fi
 
 python /data/job_pro/dataX/meeting/retryUrl.py
 if [ $? == 0 ];then
-	mv /data/work/url_error/url_error.txt /data/work/url_error/url_error_$preday.txt
-	touch /data/work/url_error/url_error.txt
+	cat /data/work/url_error/url_error.txt >> /data/work/url_error/url_error_$preday.txt
+	> /data/work/url_error/url_error.txt
 fi
 
