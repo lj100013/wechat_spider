f14d3945e27cd1482ed5334c3d39ffcd48af7cf7
chengzhaoyang <526879530@qq.com>
2019-07-19 18:57:21 +0800

    fix

--- wechat_spider/yiyaoren_spider.py
+++ wechat_spider/yiyaoren_spider.py
@@ -7,10 +7,10 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filemode='a')
 weixin = Spider()
-# weixin_names = [("赛柏蓝","INFO","yyr"),("健识局","INFO","yyr"),("药明康德","INFO","yyr"),("华招医药网","INFO","yyr"),("医招采","INFO","yyr"),("全药网","INFO","yyr"),
-#                 ("药店经理人","INFO","yyr"),("E药经理人","INFO","yyr"),("看医界","INFO","yyr"),("中国药闻","INFO","yyr"),("E药汇","INFO","yyr"),("药智网","INFO","yyr"),
-#                 ("动脉网","INFO","yyr"),("中国医药创新促进会","INFO","yyr")]
-weixin_names = [("中国医药创新促进会","INFO","yyr")]
+weixin_names = [("赛柏蓝","INFO","yyr"),("健识局","INFO","yyr"),("药明康德","INFO","yyr"),("华招医药网","INFO","yyr"),("医招采","INFO","yyr"),("全药网","INFO","yyr"),
+                ("药店经理人","INFO","yyr"),("E药经理人","INFO","yyr"),("看医界","INFO","yyr"),("中国药闻","INFO","yyr"),("E药汇","INFO","yyr"),("药智网","INFO","yyr"),
+                ("动脉网","INFO","yyr"),("中国医药创新促进会","INFO","yyr")]
+#weixin_names = [("中国医药创新促进会","INFO","yyr")]
 pool = ThreadPool(1)
 pool.map(weixin.pipeline2db, weixin_names)
 pool.close()
