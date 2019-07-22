6e5162ef98e7e3bf4483ba03e4686c67d17cf204
chengzhaoyang <526879530@qq.com>
2019-07-22 14:39:16 +0800

    fix

--- wechat_spider/yiyaoren_spider.py
+++ wechat_spider/yiyaoren_spider.py
@@ -1,17 +1,16 @@
 import logging
 from multiprocessing.dummy import Pool as ThreadPool
-from spider import Spider
+from multhread_spider import pipeline2db
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filemode='a')
-weixin = Spider()
 weixin_names = [("赛柏蓝","INFO","yyr"),("健识局","INFO","yyr"),("药明康德","INFO","yyr"),("华招医药网","INFO","yyr"),("医招采","INFO","yyr"),("全药网","INFO","yyr"),
                 ("药店经理人","INFO","yyr"),("E药经理人","INFO","yyr"),("看医界","INFO","yyr"),("中国药闻","INFO","yyr"),("E药汇","INFO","yyr"),("药智网","INFO","yyr"),
                 ("动脉网","INFO","yyr"),("中国医药创新促进会","INFO","yyr")]
-#weixin_names = [("中国医药创新促进会","INFO","yyr")]
-pool = ThreadPool(1)
-pool.map(weixin.pipeline2db, weixin_names)
+# weixin_names = [("中国医药创新促进会","INFO","yyr")]
+pool = ThreadPool(4)
+pool.map(pipeline2db, weixin_names)
 pool.close()
 pool.join()
--- wechat_spider/ysq_spider.py
+++ wechat_spider/ysq_spider.py
@@ -1,12 +1,11 @@
 import logging
 from multiprocessing.dummy import Pool as ThreadPool
-from spider import Spider
+from multhread_spider import pipeline2db
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filemode='a')
-weixin = Spider()
 weixin_names = [("好医生","QK","ysq"),("丁香园","QK","ysq"),("看医界","INFO","ysq"),("医学界消化肝病频道","NKAC","ysq"),("小大夫漫画","QK","ysq"),("消化界","NKAC","ysq"),
                 ("MedSci梅斯","QK","ysq"),("华医网","QK","ysq"),("IBD学术情报官","NKAC","ysq"),("爱肝联盟","CRAC","ysq"),("临床肝胆病杂志","WKAB","ysq"),
                 ("消化时间","NKAC","ysq"),("医脉通消化科","NKAC","ysq"),("孙锋医生","WKAC","ysq"),("基层医师公社","QK","ysq"),("医学界","QK","ysq"),
@@ -31,7 +30,7 @@ weixin_names = [("好医生","QK","ysq"),("丁香园","QK","ysq"),("看医界","
                 ("中华消化外科杂志","NKAC","ysq"),("朝阳心脏超声","NKAA","ysq"),("危重症文献学习","ZZ","ysq"),("中华重症医学电子杂志","ZZ","ysq"),("神经现实","SJ","ysq"),
                 ("神经介入资讯","SJ","ysq"),("医药魔方","INFO","ysq"),("赛博蓝","INFO","ysq"),("中洪博元医学实验帮","INFO","ysq"),("医咖会","INFO","ysq"),
                 ("生物学霸","INFO","ysq")]
-pool = ThreadPool(2)
-pool.map(weixin.pipeline2db, weixin_names)
+pool = ThreadPool(4)
+pool.map(pipeline2db, weixin_names)
 pool.close()
 pool.join()
