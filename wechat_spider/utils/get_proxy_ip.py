b3edacb1bb162e78b94d48682b2a4996dae742c0
chengzhaoyang <526879530@qq.com>
2019-07-23 16:23:07 +0800

    fix

--- wechat_spider/utils/get_proxy_ip.py
+++ wechat_spider/utils/get_proxy_ip.py
@@ -10,15 +10,17 @@ def get_ip(retrytimes):
     :return: host:port
     """
     zhi_ma_apiUrl = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&pack=34734&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions="
-    res = None
-    try:
-        while retrytimes >= 0:
+    res = ''
+    while retrytimes >= 0:
+        try:
             time.sleep(2.5)
             res = requests.get(zhi_ma_apiUrl).text
             json.loads(res)
-            logging.error("fail to get proxy ip:{}".format(res))
+            logging.warning("fail to get proxy ip:{}".format(res))
             return get_ip(retrytimes - 1)
-        else:
-            return None
-    except:
-        return res.strip('\r\n')
+        except Exception as e:
+            logging.warning(e)
+            return res.strip('\r\n')
+    logging.error("fail to get proxy ip,return None:{}".format(res))
+    return None
+    
