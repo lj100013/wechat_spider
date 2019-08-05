a22a5cd7a0e99c73d84b021e0ab427c25384b2ce
luolianyun <luoly@dachentech.com.cn>
2019-08-05 16:07:05 +0800

    【mongo2hive_yhq】新增mongo2hive医患圈相关脚本（1）

---
+++ mongo2hive/run_yhq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $2 == '' ]];then
+   preday=`date -d "yesterday" +%Y-%m-%d`
+   pre2day=`date -d "2 days ago" +%Y-%m-%d`
+else
+   preday=$2
+   pre2day=$3
+fi
+
+jobname=$1
+
+bash /data/job_pro/utils/job_flag.sh $jobname /data/job_pro/dataX/mongo2hive $preday $pre2day
