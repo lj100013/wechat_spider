a1dee6b5b1275ac906198aa52b99b1bbde968930
yangzhiyuan <yangzy@dachentech.com.cn>
2019-11-14 00:08:45 +0800

    fix

--- iot/mvn_iot.sh
+++ iot/mvn_iot.sh
@@ -3,4 +3,5 @@ source /etc/profile
 cd /data/job_pro/dataX/iot/IOTFlowAPI
 git pull
 mvn clean install
+rm /data/job_pro/dataX/iot/IOTFlowAPI.jar
 cp /data/job_pro/dataX/iot/IOTFlowAPI/target/IOTFlowAPI.jar /data/job_pro/dataX/iot 
