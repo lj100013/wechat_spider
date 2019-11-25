53f3afbc76f09e7c0470aac05ca86f22c0070df8
chenlg <chenlg@dachentech.com.cn>
2019-11-25 09:46:16 +0800

    fix

--- iot/iotflow.py
+++ iot/iotflow.py
@@ -76,12 +76,13 @@ class LLSpider(object):
                         operator = row[2]
                         statu = row[3]
                         flow = self.convert(row[4])
-                        date = "'" + row[5] + "'"
+                        date = "'" + row[5][:-2] + "'"
+                        hour = "'" + row[5][-2:] + "'"
 
-                        sql = 'upsert into dw.dw_esy_iotflow values(%s,%s,%s,concat(cast(unix_timestamp() as string),"000"))' \
-                              % (id, date, int(flow))
+                        sql = 'upsert into dw.dw_esy_iotflow values(%s,%s,%s,concat(cast(unix_timestamp() as string),"000"),%s)' \
+                              % (id, date, int(flow), hour)
                         try:
-                            ''  # self.impala_cur.execute(sql)
+                            self.impala_cur.execute(sql)
                         except Exception as e:
                             print(e)
 
